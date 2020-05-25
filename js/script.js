5b1fd853b50ba1860040e722e21f3ea75b66f6e0
Tommaso Venza <tommasovenza@gmail.com>
2020-05-25 16:49:16 +0200

    esercizio due

--- js/script.js
+++ js/script.js
@@ -1,20 +1,44 @@
-var arrayMail = ['tommasovenza@gmail.com', 'pippo-mail@mail.com', 'franchino_dj@email.it', 'paolinopaperino@gmail.com' ];
+// esercizio lista email
 
-var utenteEmail = prompt('inserisci la tua email');
-console.log(utenteEmail);
+
+var arrayMail = ['tommasovenza@gmail.com', 'pippo-mail@mail.com', 'franchino_dj@email.it', 'paolinopaperino@gmail.com'];
 
 // dichiaro di partenza la variabile falsa perchè non so se questa email la troverò nella lista di email
-untenteEmail = false;
-console.log(utenteEmail);
+
+utenteEmail = prompt('inserisci la tua email');
+
+var accesso = false;
 
 // vado a leggere l'array con un ciclo
-for (var i = 0; i < arrayMail.length; i++ ) {
-  console.log(arrayMail[i]);
+for (var i = 0; i < arrayMail.length; i++) {
 
-  if ( utenteEmail == arrayMail[i]) {
-    alert('la tua email è presente nella lista, puoi entrare!');
-  } else {
-    alert('non risulti presente nella lista. pertanto non puoi entrare');
+  if (arrayMail[i] === utenteEmail) {
+    accesso = true;
   }
+
+  if (accesso) {
+    alert('sei presente nella lista, puoi entrare');
+  } else {
+    alert('non sei presente nella lista, pertanto non puoi entrare!')
+  }
+
 }
+
+// esercizio dadi
+
+var dadoUtente = Math.floor(Math.random() * 6) + 1;
+console.log(dadoUtente);
+
+var dadoComputer = Math.floor(Math.random() * 6) + 1;
+console.log(dadoComputer);
+
+if (dadoUtente > dadoComputer) {
+  alert('hai Vinto!');
+} else if (dadoUtente == dadoComputer) {
+  alert('pareggio!');
+} else {
+  alert('ha vinto il computer!');
+}
+
+
 
