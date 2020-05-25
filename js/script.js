79daec17ab729d45342ff60ce527ab8df2036fbb
Tommaso Venza <tommasovenza@gmail.com>
2020-05-25 15:27:13 +0200

    esercizio uno

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
 var arrayMail = ['tommasovenza@gmail.com', 'pippo-mail@mail.com', 'franchino_dj@email.it', 'paolinopaperino@gmail.com' ];
 
-
 var utenteEmail = prompt('inserisci la tua email');
 console.log(utenteEmail);
 
 // dichiaro di partenza la variabile falsa perchè non so se questa email la troverò nella lista di email
-paperinoPresente = false;
-console.log(paperinoPresente);
-
-paperinoPresente = 'paolinopaperino@gmail.com';
+untenteEmail = false;
+console.log(utenteEmail);
 
 // vado a leggere l'array con un ciclo
 for (var i = 0; i < arrayMail.length; i++ ) {
   console.log(arrayMail[i]);
 
-  if ( utenteEmail === paperinoPresente) {
-    alert(paperinoPresente);
-    paperinoPresente = true;
-    console.log(paperinoPresente);
+  if ( utenteEmail == arrayMail[i]) {
+    alert('la tua email è presente nella lista, puoi entrare!');
+  } else {
+    alert('non risulti presente nella lista. pertanto non puoi entrare');
   }
 }
+
